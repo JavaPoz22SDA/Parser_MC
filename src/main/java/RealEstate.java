74272fdf1de4548e921db59e623239ae7a6475a8
mcholewa1218 <malgorzatacholewa1218@gmail.com>
2020-04-26 13:16:45 +0200

    first push

---
+++ src/main/java/RealEstate.java
@@ -0,0 +1,16 @@
+public class RealEstate {
+
+    private String street;
+    private String city;
+    private String zip;
+    private String state;
+    private int beds;
+    private int baths;
+    private int sq__ft;
+    private String type;
+    private String sale_date;
+    private double price;
+    private String latitude;
+    private String longitude;
+
+}
